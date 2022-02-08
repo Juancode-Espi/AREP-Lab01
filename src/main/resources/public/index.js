e80e9ba4d2eef10b15d7dbc4de0dd35699687089
Juan Espinosa <juan.espinosa-t@mail.escuelaing.edu.co>
2022-02-07 23:33:02 -0500

    Url y despliegue Heroku

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -4,7 +4,7 @@ var index = (function(){
     return {
         conect : function(tipo,numero){
             //var invocation = new XMLHttpRequest();
-            var url = 'http://localhost:4567/Converter/'+ tipo + '/' + numero;
+            var url = 'https://powerful-garden-62570.herokuapp.com/Converter/'+ tipo + '/' + numero;
             fetch(url)
                   .then(response => response.json())
                   .then(json => console.log(json))
